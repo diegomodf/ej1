2bfd4b42806d16fcb35d5b45ae39a5a07812e471
henriquevro <henriquevro@gmail.com>
2019-07-10 12:52:24 -0300

    Adiciona SQL do BD e Arquivo Conecta.php

---
+++ upload/conecta.php
@@ -0,0 +1,25 @@
+<?php
+
+class db{
+
+    private $host ='localhost';
+    private $usuario ='root';
+    private $senha ='';
+    private $database ='ej1_bd';
+     
+    public function conecta_mysql(){
+        //Cria conexão
+        $con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+        mysqli_set_charset($con, 'utf8');
+          
+        if(mysqli_connect_errno()){
+            //Retorna a frase + erro caso dê falha na conexão com o db
+            echo('Erro ao tentar se conectar com o banco de dados Mysql: '.mysqli_connect_error());
+                
+        }
+            //se tudo der certo, retorna conexão
+        return $con;
+    }
+}
+
+?>
